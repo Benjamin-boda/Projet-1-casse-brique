87fb927496e4eed594b7ec500ffaa7b7fc406851
Benjamin-boda <benjamiin.boda@gmail.com>
2020-06-02 22:39:43 +0200

    Déplacement verticaux

--- cassebrique.js
+++ cassebrique.js
@@ -12,12 +12,13 @@ blocElt.style.width = "75px"
 blocElt.style.height = "15px"
 blocElt.style.backgroundColor = "blue";
 blocElt.style.position = "relative";
-blocElt.style.left = "0px";
+blocElt.style.left = (parseInt(cadreElt.style.width) - parseInt(blocElt.style.width))/2 + "px";
 blocElt.style.bottom = "-425px";
-blocElt.style.marginLeft = "5px";
+
+
 
 function moveLeft() {
-    if (parseInt(blocElt.style.left) <= -5) {
+    if (parseInt(blocElt.style.left) <= 0) {
         blocElt.style.left = parseInt(blocElt.style.left) -0 + "px";
     } else {
         blocElt.style.left = parseInt(blocElt.style.left) -5 + "px";
@@ -25,11 +26,27 @@ function moveLeft() {
 }
 
 function moveRight() {
-    if (parseInt(blocElt.style.left) >= 770) {
+    if (parseInt(blocElt.style.left) >= parseInt(cadreElt.style.width) - parseInt(blocElt.style.width)) {
         blocElt.style.left = parseInt(blocElt.style.left) -0 + "px";
     } else {
         blocElt.style.left = parseInt(blocElt.style.left) +5 + "px";
     }
+}
+
+function moveUp() {
+    if (parseInt(blocElt.style.bottom) >= 0) {
+    blocElt.style.bottom = parseInt(blocElt.style.bottom) -0 + "px";
+    } else {
+        blocElt.style.bottom = parseInt(blocElt.style.bottom) +5 + "px";
+    }  
+}
+
+function moveDown() {
+    if (parseInt(blocElt.style.bottom) <= -parseInt(cadreElt.style.height) + parseInt(blocElt.style.height)) {
+        blocElt.style.bottom = parseInt(blocElt.style.bottom) -0 + "px";
+    } else {
+        blocElt.style.bottom = parseInt(blocElt.style.bottom) -5 + "px";
+    } 
 }
 
 bodyElt.addEventListener("keydown", function (e) {
@@ -39,14 +56,23 @@ bodyElt.addEventListener("keydown", function (e) {
             moveLeft();
             console.log("gauche");
             break
+        case 38:
+            moveUp();
+            console.log("haut");
+            break    
         case 39:
             moveRight();
             console.log("droite");
+            break
+        case 40:
+            moveDown();
+            console.log("bas");
             break
         default :
             console.log("Touche " + touche + " non gérée");
     }
 })
+
 
 
 
